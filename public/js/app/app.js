f77f072ead3be5facc0938bf16e759f1751f53f1
hokutosei <jeanepaul@gmail.com>
2015-03-11 10:42:41 +0900

    update kickstart web framework frontend

--- public/js/app/app.js
+++ public/js/app/app.js
@@ -1,3 +1,21 @@
 'use strict';
 
 var app = angular.module('web102', ['ngRoute', 'ngResource']);
+
+app.config(function($routeProvider, $locationProvider) {
+
+	$routeProvider
+		.when('/', {
+			templateUrl: 'js/app/index/template/' + 'index.html',
+			controller: 'IndexController'
+		})
+		.otherwise({
+			redirectTo: '/'
+		})
+
+	$locationProvider.html5Mode({
+		enabled: false,
+		requireBase: false
+	})
+
+})
---
+++ public/js/app/index/index_controller.js
@@ -0,0 +1,8 @@
+(function() {
+	'use strict';
+	var log = function(str) { console.log(str); };
+
+	app.controller('IndexController', ['$scope', function($scope) {
+		log("IndexController")
+	}])
+}())

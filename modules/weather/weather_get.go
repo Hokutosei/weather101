acfe93d2ece0803504ba10d71290af087f91fd63
hokutosei <jeanepaul@gmail.com>
2015-03-24 16:44:11 +0900

    get request implementation

--- modules/weather/weather_get.go
+++ modules/weather/weather_get.go
@@ -2,22 +2,53 @@ package weather
 
 import (
 	"fmt"
-	"net/http"
+	_ "net/http"
+	"time"
+	"io/ioutil"
+	"encoding/json"
 )
 
 var (
 	api_url = "http://api.openweathermap.org/data/2.5/weather?q="
+	delay = 4
 )
 
 func getWeather(city ...string) {
 	for _, name := range city {
 		go func(name string) {
-			fmt.Println(name)
+			city_url := fmt.Sprintf("%v%v", api_url, name)
+
+			// http request
+			response := httpGet(city_url)
+			defer response.Body.Close()
+
+			// read response
+			contents, err := ioutil.ReadAll(response.Body)
+			if err!= nil {
+				fmt.Println(err)
+				return
+			}
+
+			// put response data in struct
+			var dat WeatherData
+			if err := json.Unmarshal(contents, &dat); err != nil {
+				fmt.Println(err)
+				return
+			}
+
+			fmt.Println(dat)
+
+			fmt.Println(response)
+
 		}(name)
 	}
 }
 
 func StartGettingWeather() {
-	cities := []string{ "akiruno-shi", "paranaque", "omiya-shi", "machida-shi" }
-	getWeather(cities...)
+
+	for i := range time.Tick(time.Second * time.Duration(delay)) {
+		_ = i
+		cities := []string{ "akiruno-shi", "paranaque", "omiya-shi", "machida-shi" }
+		getWeather(cities...)		
+	}
 }

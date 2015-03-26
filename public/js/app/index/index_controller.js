babd42bc436dcddd5eaa9d18f771280c20deb597
hokutosei <jeanepaul@gmail.com>
2015-03-26 13:03:33 +0900

    display city data in reactjs implementation

--- public/js/app/index/index_controller.js
+++ public/js/app/index/index_controller.js
@@ -3,10 +3,10 @@
 	var log = function(str) { console.log(str); };
 
 	app.controller('IndexController', ['$scope', 'indexService', function($scope, indexService) {
-		log("IndexController")
         indexService.getIndexData(function(data, status) {
             log(status)
             log(data)
+            $scope.city_data = data;
         })
 	}])
 }())
